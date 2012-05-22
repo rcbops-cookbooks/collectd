d3b0c6f446a8502d753610f9d808245bd3c24d86
Darren Birkett <darren.birkett@gmail.com>
2012-05-22 14:07:01 +0100

    extending behaviour
    - beginning of adding support for fedora 17
    - add attribute toggle to allow forwarding to a remote collectd server
    - include syslog plugin by default for client and server

--- recipes/server.rb
+++ recipes/server.rb
@@ -7,9 +7,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,14 @@
 
 include_recipe "collectd"
 
-collectd_plugin "network" do
-  options :listen=>'0.0.0.0'
+  collectd_plugin "network" do
+  if node['collectd']['is_proxy']
+    options :listen=>'0.0.0.0', :server=>node['collectd']['remote']['ip'], :Forward=>'True'
+  else
+    options :listen=>'0.0.0.0'
+  end
+end
+
+collectd_plugin "syslog" do
+  options :log_level => "info"
 end
